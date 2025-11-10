f7de684c9e7f40726616db135b258714703aa34c
Luciana Almeida <luciana.almeida@ifce.edu.br>
2025-11-10 13:31:19 -0300

    Componentes stateless e stateful

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Olá mundo!",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 50),
+      ),
+    );
+  }
+}
